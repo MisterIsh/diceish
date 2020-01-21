cf5f7727344cd6aca6b8765e09f8cc5ef1f9d0eb
MisterIsh <cameron.isherwood@gmail.com>
2020-01-21 16:03:37 -0500

    *

---
+++ docs/precache-manifest.e0435d0c11add1dd7897ec63bb2fd7d4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b273cd22217fe077f823",
+    "url": "/diceish/css/chunk-vendors.73a17ade.css"
+  },
+  {
+    "revision": "fed813515f5743afe63b42bd51fea7f0",
+    "url": "/diceish/favicon-16x16.png"
+  },
+  {
+    "revision": "754e9970947973115b6d4c8aeb10120d",
+    "url": "/diceish/favicon-32x32.png"
+  },
+  {
+    "revision": "165431f583272bbe4e3d1a21f0a3d372",
+    "url": "/diceish/img/diceIshWhite.165431f5.svg"
+  },
+  {
+    "revision": "8608e01f35223108645be0a763cb89da",
+    "url": "/diceish/index.html"
+  },
+  {
+    "revision": "8c627f7a972633f1f1b8",
+    "url": "/diceish/js/app.4fda9bb3.js"
+  },
+  {
+    "revision": "b273cd22217fe077f823",
+    "url": "/diceish/js/chunk-vendors.538a96c6.js"
+  },
+  {
+    "revision": "1678fd2d39036427e9841734b58e93a0",
+    "url": "/diceish/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/diceish/robots.txt"
+  }
+]);
