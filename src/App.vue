70abb6fec460ffa1bd30230524a645c8409ac24c
MisterIsh <cameron.isherwood@gmail.com>
2020-01-21 16:11:36 -0500

    *

--- src/App.vue
+++ src/App.vue
@@ -7,21 +7,12 @@
     >
       <div class="d-flex align-center">
         <v-img
-          alt="Vuetify Logo"
+          alt="Dice-Ish Logo"
           class="shrink mr-2"
           contain
           src="~@/assets/diceIshWhite.svg"
           transition="scale-transition"
           width="120"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
         />
       </div>
 
